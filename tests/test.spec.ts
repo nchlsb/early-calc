9f6ba89c13c1160a0d61b6d40cce6472ec746b08
nchlsb <nchlsbrdy@gmail.com>
2020-12-22 17:16:53 -0500

    Added some unit tests for range

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -2,8 +2,23 @@ import { range } from '../src/helpers';
 import { expect } from 'chai';
 import 'mocha';
 
-describe('First test', () => { 
-    it('test', () => { 
-      expect(range(2)).to.equal([0, 1]); 
-  }); 
+describe("Range", () => {
+  it("Should have length equal to the value of it's argument", () => {
+    expect(range(10)).to.have.length(10);
+  });
+
+  it("Should a last element equal to the value of it's argument minus one", () => {
+    const argument = 10;
+    const result = range(argument);
+    expect(result[result.length - 1]).to.equal(argument - 1);
+  });
+
+  it("Should have elelments that sum to nth triangluar number where n is the arugment - 1", () => {
+    const n = 10;
+    const m = n - 1
+    expect(range(n).reduce((sum, current) => sum + current)).to.equal((m * (m +1)) / 2);
+  });
+
+
+
 });
