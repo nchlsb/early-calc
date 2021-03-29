f05682f5e7980381f62c6573714469495eba9162
nchlsb <nchlsbrdy@gmail.com>
2021-03-29 19:27:49 -0400

    Added a very simple unit test for the function point

--- tests/helpers.spec.ts
+++ tests/helpers.spec.ts
@@ -34,6 +34,7 @@ describe("slope", () => {
 
   it("Should work for a negative negative", () => {
     expect(slope(point(0, 0), point(1, -1))).to.equal(-1);
+    expect(slope(point(0, 0), point(1, -1))).to.equal(-1);
   });
 
   it("Should work for a slope of zero", () => {
@@ -68,5 +69,11 @@ describe("twoPointForm", () => {
 
   it("Should not work for (return Infinity) if it takes the two points witht the same x value but differnt y value.", () => {
     expect(isFinite(twoPointForm(point(1, 1), point(1, 1))(2))).to.equal(false);
+  });
+});
+
+describe("point", () => {
+  it("Should create a point with the given x and y values", () => {
+    expect(point(0, 0)).to.deep.equal({x: 0, y: 0});
   });
 });
